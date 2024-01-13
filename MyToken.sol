626d0aba60a62defe50dafface6bb4c2facb621b
gangstarr60 <sukrucildirr@gmail.com>
2024-01-13 17:03:45 +0300

    berachain-artio-test

---
+++ MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+}
